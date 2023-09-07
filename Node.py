1d69fa791a0ec1c66b975bb27e79d17eb6715270
Lauren Altman <laltman2@setups-MacBook-Pro-34.local>
2023-09-07 15:19:45 -0400

    print str for node

--- Node.py
+++ Node.py
@@ -8,6 +8,9 @@ class Node():
         self.position = position
         self.fixed = fixed
 
+    def __str__(self):
+        return f'Node index: {self.index}, position: {self.position}, fixed:{self.fixed}'
+
 if __name__=='__main__':
     n = Node()
     print(n.index, n.position, n.fixed)
